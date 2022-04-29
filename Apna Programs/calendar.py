54acc1aadd330e912a99a772e50ee8d71960eabe
Prithwijit Ghosh <101276010+BasiliskFTW@users.noreply.github.com>
2022-04-29 21:36:25 +0530

    Add files via upload

---
+++ Apna Programs/calendar.py
@@ -0,0 +1,4 @@
+import calendar
+year =int( input("Enter the year of the required calendar "))
+month = float( input("Enter the month of the required calendar "))
+print(calendar.calendar(year))                          
